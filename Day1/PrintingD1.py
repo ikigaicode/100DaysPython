1e83391ee97399a67b3d80cd68b1a34db92dd82e
ikigaicode <academychilton@gmail.com>
2025-08-05 04:02:33 -0700

    Using print() function

--- Day1/PrintingD1.py
+++ Day1/PrintingD1.py
@@ -3,3 +3,9 @@
 # Code will be writing below this line.
 
 print("Hello Terra!")
+
+# I didn't use "Hello World!" because I don't want to write the same line as in the course.
+# The format to print things in Python is:
+
+# print("some text")
+
