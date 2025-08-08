aa854ace7d08f63a33f1bacf5e3fd750a04d9322
ikigaicode <academychilton@gmail.com>
2025-08-07 20:21:49 -0700

    Using concatenation to joint strings, but respecting spaces between words.

--- Day1/03_StringManipulation.py
+++ Day1/03_StringManipulation.py
@@ -1,3 +1,27 @@
 # Writing the code below this line.
 
 print("Hello Terra!\nHello Terra!\nHello Terra!")
+
+
+# Learning to use -String concatenation-
+
+print("Hello" + "Artemis")
+
+# we notice that both strings are printed as one single word. There is no space between both words.
+
+#We need to add a -space character-between both words.
+
+#There are tree option we can apply:
+#The first one is adding a space in the string "Hello", before the end quotation mark of the first string.
+
+print("Hello " + "Artemis")
+
+# The second option is adding a space at the beginning of the second string "Artemis", before the letter A.
+
+print("Hello" + " Artemis")
+
+# The third option is adding a -space string-, between the last two. Using concatenation. As follows:
+
+print("Hello" + " " + "Artemis")
+
+#---
