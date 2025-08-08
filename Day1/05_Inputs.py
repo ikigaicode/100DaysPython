70d18bcdcbaa3588b801b4f962fa69435c97a096
ikigaicode <academychilton@gmail.com>
2025-08-08 14:34:32 -0700

    Learning about input() function.

---
+++ Day1/05_Inputs.py
@@ -0,0 +1,16 @@
+#Learning to use the Python input() function to collect user input and use it within the code.
+
+#The initial line code is:
+print("Hello" + " " + "Artemis")
+
+#But I want to enter some data. Asking a question.
+print("What is your name?")
+
+#But there is noway the user can give an input to the program to use it.
+
+#In order to do that, I need to use another function. The input() function.
+input("What is your name?")
+
+#Now we are going to use concatenation to join the input and the print function.
+
+print("Hello " + input("What is your name?"))
