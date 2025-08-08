a5e2b3ff3da3a77cb54a468a16b2c464d85b3e91
ikigaicode <academychilton@gmail.com>
2025-08-07 21:35:54 -0700

    Using Debugging in five lines of code. Fixing SyntaxWarning: invalid escape sequence.

--- Day1/04_DebuggingPractice.py
+++ Day1/04_DebuggingPractice.py
@@ -4,8 +4,21 @@ print("Notes from Day 1")
 print("The print statement is used to output strings")
 print("Strings are strings of characters")
 print("String Concatenation is done with the + sign")
-print("New lines can be created with a \ and the letter n")
 
+
+# While this line is correct:
+#print("New lines can be created with a \ and the letter n")
+
+#While running the code, it shows an error:
+#SyntaxWarning: invalid escape sequence '\ ' print("New lines can be created with a \ and the letter n")
+
+#I search such error on Google, and I came to learn that:
+
+#Escape the Backslash: To include a literal backslash in a regular string, escape it by using a double backslash (\\).
+#Python:
+# print("This is a backslash: \\")
+
+print("New lines can be created with a \\ and the letter n")
 
 
 
