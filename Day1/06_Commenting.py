48e01bca36273345a623be0accb5feec45c57c4e
ikigaicode <academychilton@gmail.com>
2025-08-08 17:56:44 -0700

    Form to write comments in lines so the computer doesn't execute it.

---
+++ Day1/06_Commenting.py
@@ -0,0 +1,11 @@
+# If I want a line of text to not be considered by the computer at all,
+# all I have to do is add the hashtag sign #
+# or pound sign
+# in front of the text.
+# and this turns it into a comment.
+
+# Like this. Yes, THIS text. line 1, 2, 3, 4, 5 and 7.
+
+#Something the computer will completely ignore. And I can write code even, as much as I like, but it won't be executed.
+
+# This way I will write notes for myself to explain to me what I was doing.
