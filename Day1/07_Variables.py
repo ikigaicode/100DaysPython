fbbcf5040a94553f98186935a854459f97530767
ikigaicode <academychilton@gmail.com>
2025-08-08 19:59:31 -0700

    Variable name in Python

---
+++ Day1/07_Variables.py
@@ -0,0 +1,16 @@
+# Lesson about Python variables.
+
+# name is the variable then I will be able to refer to it later. This assigns whatever the user typed in as input to a variable called name.
+name = input("What is your name?")
+print(name)
+
+#Then another way of writing a variable.
+
+name = "Joshua"
+print(name)
+
+# Now I give another name to the variable name,
+
+name = "Artemis"
+print(name)
+
