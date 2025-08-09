e2222e1fc3bc500720853b470eed59cd11dc6b49
ikigaicode <academychilton@gmail.com>
2025-08-08 20:21:59 -0700

    len() function in Python. Also, creating variables to use functions.

---
+++ Day1/08_lenFunction.py
@@ -0,0 +1,24 @@
+#Checking the length of the user input.
+
+# How many characters there are in the name of what is type in?
+
+name = "James"
+print(len(name))
+print()
+name = "Joy"
+print(len(name))
+print()
+print()
+# Now using the input function; calculate the length, using the length function, and last the print function.
+print(len(input("What ir your name?")))
+print()
+print()
+# Next I will separate the two functions, input() and len() and later print what the user type in the input function.
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+
+
+
+
