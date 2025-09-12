b01cf18a0ad293b0cb3a91e59be657a241efa946
ikigaicode <academychilton@gmail.com>
2025-09-11 17:42:11 -0700

    Variable names in Python.

--- Day1/10_VariableNaming.py
+++ Day1/10_VariableNaming.py
@@ -3,3 +3,13 @@
 name = "Artemis"
 lenght = len(name)
 print(lenght)
+
+#Also, we can changed the variable names as:
+
+n = "Alvaro"
+l = len(n)
+print(l)
+
+#But is not recommended, because when someone need to check the code, it will be difficult to understand what it is doing.
+# So, we must be more explicit with variables' names.
+
